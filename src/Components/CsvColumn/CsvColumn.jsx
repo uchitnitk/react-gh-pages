0a7c991ccd9937302ec30190c2c52c57334a8120
uchit kumar <uchitnitk@gmail.com>
2021-08-04 12:09:17 +0530

    added images and adjusted csv column

--- src/Components/CsvColumn/CsvColumn.jsx
+++ src/Components/CsvColumn/CsvColumn.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import TableCell from '@material-ui/core/TableCell';
-import Link from '@material-ui/core/Link';
+import React from "react";
+import TableCell from "@material-ui/core/TableCell";
+import Link from "@material-ui/core/Link";
+import logo from "../../images/file.png";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+  },
+  image: {
+    padding: "10px",
+    height: "25px",
+    width: "20px",
+  },
+});
 
 const CsvColumn = (props) => {
-        return (
-                <TableCell key={props.key} align={props.align}>
-					<Link href="#" >
-							CSV
-					</Link>
-                </TableCell>
-        );
-}
+  const classes = useStyles();
+  return (
+    <TableCell key={props.key} align={props.align}>
+      <div className={classes.root}>
+        <img src={logo} alt="logo" className={classes.image} />
+        <Link href="#" color="Primary" underline="none">
+          CSV
+        </Link>
+      </div>
+    </TableCell>
+  );
+};
 export default CsvColumn;
