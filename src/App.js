d6372f8fe3ea9b59d935b9ffc9141ea616368adc
uchit kumar <uchitnitk@gmail.com>
2021-08-09 22:32:05 +0530

    Basic setup of localization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from 'react';
-import Header from './Components/Header/Header'
-import TabView from  './Components/TabView/TabView'
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <TabView />
-    </div>
-  );
+import React from "react";
+import { withTranslation } from "react-i18next";
+import Header from "./Components/Header/Header";
+import TabView from "./Components/TabView/TabView";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "en",
+    };
+  }
+
+  onLanguageHandle = (event) => {
+    let newLang = event.target.value;
+    this.setState({ value: newLang });
+    this.props.i18n.changeLanguage(newLang);
+  };
+
+  renderRadioButtons = () => {
+    return (
+      <div>
+        <input
+          checked={this.state.value === "en"}
+          name="language"
+          onChange={(e) => this.onLanguageHandle(e)}
+          value="en"
+          type="radio"
+        />
+        English &nbsp;
+        <input
+          name="language"
+          value="jp"
+          checked={this.state.value === "jp"}
+          type="radio"
+          onChange={(e) => this.onLanguageHandle(e)}
+        />
+        Japanese &nbsp;
+        <input
+          name="language"
+          value="de"
+          checked={this.state.value === "de"}
+          type="radio"
+          onChange={(e) => this.onLanguageHandle(e)}
+        />
+        German
+      </div>
+    );
+  };
+  render() {
+    return (
+      <div className="App">
+        {this.renderRadioButtons()}
+        <Header />
+        <TabView />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withTranslation()(App);
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { withTranslation } from "react-i18next";
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
-    paddingLeft: '10%',
-    paddingTop: '5%'
+    width: "100%",
+    paddingLeft: "10%",
+    paddingTop: "5%",
   },
 });
 
-const Header = () => {
-        const classes = useStyles();
-        return (      
-                <div className={classes.root}>
-                <Typography variant="h3" component="h2" gutterBottom>
-                        Manage Campaigns
-                </Typography>
-                </div>
-        );
-}
+const Header = (props) => {
+  const { t } = props;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant="h3" component="h2" gutterBottom>
+        {t("heading")}
+      </Typography>
+    </div>
+  );
+};
 
-export default Header;
+export default withTranslation()(Header);
